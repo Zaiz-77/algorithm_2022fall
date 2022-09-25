499d531279fdc7b1c4acfd69044a33c0c5aa374f
Zaiz <80518792+Zaiz-77@users.noreply.github.com>
2022-09-25 23:05:10 +0800

    Update LCS.java

--- lectures/week03/LCS.java
+++ lectures/week03/LCS.java
@@ -2,55 +2,63 @@ package com.algorithm.lectures.week03;
 
 import java.util.*;
 
-public class Lease {
+public class LCS {
     private final StringBuilder res = new StringBuilder();
 
     /**
-     * Get the way from l to r
+     * Get the LCS from (i, j) to (0, 0)
      *
      * @param p the position array
-     * @param l start point
-     * @param r end point
+     * @param i the x-axis pos
+     * @param j the y-axis pos
+     * @param s result
      */
-    private void dfs(int[][] p, int l, int r) {
-        res.append(r + 1);
-        if (l != r) dfs(p, l, p[l][r]);
+    private void dfs(int[][] p, int i, int j, String s) {
+        if (i == 0 && j == 0) {
+            System.out.println(res.reverse());
+            return;
+        }
+        if (p[i][j] == 0) {
+            res.append(s.charAt(i - 1));
+            dfs(p, i - 1, j - 1, s);
+        } else if (p[i][j] == -1) {
+            dfs(p, i - 1, j, s);
+        } else dfs(p, i, j - 1, s);
     }
 
     /**
-     * Get the min cost from 1 to n
+     * Calc the length of LCS of s1 and s2
      *
-     * @param a the cost board
-     * @return the min cost
+     * @param s1 the first string
+     * @param s2 the second string
+     * @return the length of LCS of s1 and s2
      */
-    public int minCost(int[][] a) {
-        int n = a.length;
-        int[][] f = new int[n][n], p = new int[n][n];
-        System.arraycopy(a, 0, f, 0, n);
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                p[i][j] = i;
-                for (int k = i; k <= j; k++) {
-                    if (f[i][k] + f[k][j] < f[i][j]) {
-                        f[i][j] = f[i][k] + f[k][j];
-                        p[i][j] = k;
-                    }
+    public int longestSubSequence(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        int[][] p = new int[m + 1][n + 1];
+        int[][] f = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    f[i][j] = f[i - 1][j - 1] + 1;
+                    p[i][j] = 0;
+                } else if (f[i - 1][j] >= f[i][j - 1]) {
+                    f[i][j] = f[i - 1][j];
+                    p[i][j] = -1;
+                } else if (f[i][j - 1] >= f[i - 1][j]) {
+                    f[i][j] = f[i][j - 1];
+                    p[i][j] = 1;
                 }
             }
         }
-        dfs(p, 0, n - 1);
-        System.out.println(res.reverse());
-        return f[0][n - 1];
+        dfs(p, m, n, s1);
+        return f[m][n];
     }
 
     public static void main(String[] args) {
-        int[][] a = {
-                {0, 3, 15, 30},
-                {0, 0, 2, 16},
-                {0, 0, 0, 1},
-                {0, 0, 0, 0}
-        };
-        Lease solve = new Lease();
-        System.out.println(solve.minCost(a));
+        String s1 = "apple", s2 = "application";
+        LCS solve = new LCS();
+        System.out.println(solve.longestSubSequence(s1, s2));
     }
 }
