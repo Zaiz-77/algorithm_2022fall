b298c0251f5cf534951d57ea3cc7cd7fbfc18116
Zaiz <80518792+Zaiz-77@users.noreply.github.com>
2022-09-25 23:05:25 +0800

    Update Lease.java

--- lectures/week03/Lease.java
+++ lectures/week03/Lease.java
@@ -3,28 +3,51 @@ package com.algorithm.lectures.week03;
 import java.util.*;
 
 public class Lease {
-    public long minCost(int[][] a) {
-        int n = a.length;
-        long[][] f = new long[n][n];
+    private final StringBuilder res = new StringBuilder();
 
-        // 1 2 3 4 5
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
-                if (i == j) f[i][j] = 0;
-                else f[i][j] = a[i][i + 1] + f[i + 1][j];
-                for (int k = i; k <= j && k + 1 < n; k++) {
-                    f[i][j] = Math.min(f[i][j], Math.min(a[i][j], f[i][k] + f[k][j]));
+    /**
+     * Get the way from l to r
+     *
+     * @param p the position array
+     * @param l start point
+     * @param r end point
+     */
+    private void dfs(int[][] p, int l, int r) {
+        res.append(r + 1);
+        if (l != r) dfs(p, l, p[l][r]);
+    }
+
+    /**
+     * Get the min cost from 1 to n
+     *
+     * @param a the cost board
+     * @return the min cost
+     */
+    public int minCost(int[][] a) {
+        int n = a.length;
+        int[][] f = new int[n][n], p = new int[n][n];
+        System.arraycopy(a, 0, f, 0, n);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                p[i][j] = i;
+                for (int k = i; k <= j; k++) {
+                    if (f[i][k] + f[k][j] < f[i][j]) {
+                        f[i][j] = f[i][k] + f[k][j];
+                        p[i][j] = k;
+                    }
                 }
             }
         }
+        dfs(p, 0, n - 1);
+        System.out.println(res.reverse());
         return f[0][n - 1];
     }
 
     public static void main(String[] args) {
         int[][] a = {
-                {0, 5, 15, 30},
-                {0, 0, 7, 16},
-                {0, 0, 0, 20},
+                {0, 3, 15, 30},
+                {0, 0, 2, 16},
+                {0, 0, 0, 1},
                 {0, 0, 0, 0}
         };
         Lease solve = new Lease();
