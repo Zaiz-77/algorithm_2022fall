469d43c7be8ba07a021130677887e74e00658930
zaizwk <kandqwangkai@163.com>
2023-04-16 00:18:59 +0800

    fix dijkstra

--- lectures/week04/Dijkstra.java
+++ lectures/week04/Dijkstra.java
@@ -25,31 +25,29 @@ public class Dijkstra {
      */
     public int dijkstra(int[][] es, int src, int dst) {
         int n = es.length, M = 1141567918;
-        int[] d = new int[n];
+        int[] dis = new int[n];
         boolean[] vi = new boolean[n];
-        Arrays.fill(d, M);
-        d[src] = 0;
-        vi[src] = true;
+        Arrays.fill(dis, M);
+        dis[src] = 0;
         PriorityQueue<Node> q = new PriorityQueue<>(Comparator.comparingInt(x -> x.len));
-        Node st = new Node(src, 0);
-        q.add(st);
-
+        q.offer(new Node(src, 0));
         while (!q.isEmpty()) {
             Node now = q.poll();
-            int from = now.no, cost = now.len;
-            vi[from] = true;
-            for (int to = 0; to < n; to++) {
-                if (es[from][to] != -1 && !vi[to]) {
-                    Node nxt = new Node(to, cost + es[from][to]);
-                    if (nxt.len < d[to]) {
-                        d[to] = nxt.len;
-                        q.add(nxt);
+            int i = now.no;
+            if (vi[i]) continue;
+            vi[i] = true;
+            for (int j = 0; j < n; j++) {
+                if (es[i][j] > 0) {
+                    Node nxt = new Node(j, dis[i] + es[i][j]);
+                    if (nxt.len < dis[j]) {
+                        dis[j] = nxt.len;
+                        q.offer(nxt);
                     }
                 }
             }
         }
 
-        return d[dst];
+        return dis[dst];
     }
 
     public static void main(String[] args) {
